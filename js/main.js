f2f0f057a5405531e1ce4bac2f518a8a6444e064
RomaMbappe <romafrontend@mail.ru>
2022-09-28 18:47:09 +0400

    seebeedee

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
 $(document).ready(() => {
   $('#search-dropdown').dropkick();
 
+  $('.click-modal').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg').fadeIn(500);
+    // $('html').addClass('no-scroll');
+  });
+
+  $('.close-popup').click(function() {
+    $('.popup-bg').fadeOut(500);
+    // $('html').removeClass('no-scroll');
+  });
 
   if($(document).width() <= '720') {
     $('.menuDrop__heading').click(function() {
