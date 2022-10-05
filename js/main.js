bb277222672e822fdf51e8e58b4beadf6438cf6d
RomaMbappe <romafrontend@mail.ru>
2022-10-05 18:55:31 +0400

    seebeedee

--- js/main.js
+++ js/main.js
@@ -4,12 +4,24 @@ $(document).ready(() => {
   $('.click-modal').click(function(e) {
     e.preventDefault();
     $('.popup-bg').fadeIn(500);
+    $('.popup').fadeIn(500);
     // $('html').addClass('no-scroll');
   });
 
   $('.close-popup').click(function() {
     $('.popup-bg').fadeOut(500);
+    $('.popup').fadeOut(500);
     // $('html').removeClass('no-scroll');
+  });
+
+  $('.popup-bg').click(function() {
+    $('.popup-bg').fadeOut(500);
+    $('.popup').fadeOut(500);
+  });
+
+  $('.desc-adaptive').click(function() {
+    $('.posts-table-two').slideToggle();
+    $('.desc-adaptive__icon').toggleClass('click-icon');
   });
 
   if($(document).width() <= '720') {
@@ -76,6 +88,8 @@ $(document).ready(() => {
         breakpoint: 600,
         settings: {
           slidesToShow: 1,
+          dots: true,
+          dotsClass: "my-dots",
           appendArrows: $('.bestSlider__arrows'),
           centerMode: false,
         }
@@ -85,6 +99,8 @@ $(document).ready(() => {
         breakpoint: 320,
         settings: {
           slidesToShow: 1,
+          dots: true,
+          dotsClass: "my-dots",
           appendArrows: $('.bestSlider__arrows'),
           centerMode: false,
         }
@@ -111,6 +127,17 @@ $(document).ready(() => {
         breakpoint: 951,
         settings: {
           slidesToShow: 1,
+          // centerMode: true,
+          appendArrows: $('.revSlider__arrows')
+        }
+      },
+
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          dots: true,
+          dotsClass: "my-dots-two",
           // centerMode: true,
           appendArrows: $('.revSlider__arrows')
         }
